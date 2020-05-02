7416331340abcac33258fb86b8513a5b453f030b
ch.mao <ch.mao@chmao-Mac.local>
2020-05-02 10:57:49 +0800

    complete project

--- src/pages/project/components/environment-form/index.tsx
+++ src/pages/project/components/environment-form/index.tsx
@@ -4,51 +4,66 @@ import { useSelector } from '@/modal';
 import { IEnvsItem } from '@/services/project';
 const Option = Select.Option;
 
-export function EnvironmentForm({ formData, onChange }: { formData: IEnvsItem; onChange: (data: IEnvsItem) => void; }) {
+export function EnvironmentForm({
+  formData,
+  onChange
+}: {
+  formData: IEnvsItem;
+  onChange: (data: IEnvsItem) => void;
+}) {
   const { list } = useSelector('ssh');
 
-  const onChangeValue = useCallback(<T extends keyof IEnvsItem>(name: T, value: IEnvsItem[T]) => {
-    onChange({
-      ...formData,
-      [name]: value
-    });
-  }, [formData, onChange]);
+  const onChangeValue = useCallback(
+    <T extends keyof IEnvsItem>(name: T, value: IEnvsItem[T]) => {
+      onChange({
+        ...formData,
+        [name]: value
+      });
+    },
+    [formData, onChange]
+  );
 
   useEffect(() => {
     const sshId = list[0].ssh_id;
     if (!formData.ssh_id && sshId) {
       onChangeValue('ssh_id', sshId);
     }
-  }, [formData.ssh_id, list, onChangeValue]);
+  });
 
   return (
     <>
       <Form.Item label="环境名称">
         <Input
           value={formData.name}
-          onChange={(e) => onChangeValue('name', e.target.value)}
+          onChange={e => onChangeValue('name', e.target.value)}
           placeholder="环境名称"
         />
       </Form.Item>
       <Form.Item label="部署分支">
         <Input
           value={formData.branch}
-          onChange={(e) => onChangeValue('branch', e.target.value)}
+          onChange={e => onChangeValue('branch', e.target.value)}
           placeholder="部署分支"
         />
       </Form.Item>
       <Form.Item label="环境变量">
         <Input.TextArea
           value={formData.variables}
-          onChange={(e) => onChangeValue('variables', e.target.value)}
+          onChange={e => onChangeValue('variables', e.target.value)}
           placeholder="环境变量"
         />
       </Form.Item>
       <Form.Item label="服务器SSH">
-        <Select value={formData.ssh_id} onChange={(value) => onChangeValue('ssh_id', value)} style={{ width: 120 }}>
-          {
-            list.map(item => <Option key={item.ssh_id} value={item.ssh_id}>{item.name}</Option>)
-          }
+        <Select
+          value={formData.ssh_id}
+          onChange={value => onChangeValue('ssh_id', value)}
+          style={{ width: 120 }}
+        >
+          {list.map(item => (
+            <Option key={item.ssh_id} value={item.ssh_id}>
+              {item.name}
+            </Option>
+          ))}
         </Select>
       </Form.Item>
     </>
--- src/pages/project/components/project-modal/index.tsx
+++ src/pages/project/components/project-modal/index.tsx
@@ -20,28 +20,30 @@ const defaultEnv = {
   branch: 'master',
   created_at: 0,
   updated_at: 0,
-  deleted_at: 0,
+  deleted_at: 0
 };
 
-export function ProjectModal({ showBtn, formData = {
-  created_at: 1587306285,
-  deleted_at: 0,
-  desc: "",
-  git_path: "git@github.com:m-Ryan/react-admin.git",
-  name: "admin-后台",
-  project_id: 0,
-  repository_name: "react-admin",
-  updated_at: 1587306285,
-  user_id: 1,
-  environments: [
-    defaultEnv
-  ]
-} }: { showBtn: React.ReactNode; formData?: Project; }) {
+export function ProjectModal({
+  showBtn,
+  formData = {
+    created_at: 1587306285,
+    deleted_at: 0,
+    desc: '',
+    git_path: 'git@github.com:m-Ryan/react-admin.git',
+    name: 'admin-后台',
+    project_id: 0,
+    repository_name: 'react-admin',
+    updated_at: 1587306285,
+    user_id: 1,
+    environments: [defaultEnv]
+  }
+}: {
+  showBtn: React.ReactNode;
+  formData?: Project;
+}) {
   const { loadingMap } = useSelector('loading');
   const [visible, setVisible] = useState(false);
-  const { form, createInput, verify, setForm } = useForm<Project>(
-    formData
-  );
+  const { form, createInput, verify, setForm } = useForm<Project>(formData);
   const { create, update } = useSelector('project');
   const { getList, list } = useSelector('ssh');
 
@@ -66,67 +68,88 @@ export function ProjectModal({ showBtn, formData = {
     } else {
       create(form);
     }
-
   }, [create, form, update, verify]);
 
-  const onEdit = useCallback((targetKey: string | React.MouseEvent<HTMLElement, MouseEvent>, action: "add" | "remove") => {
-    if (action === 'add') {
-      setForm(newForm => {
-        const nameList = new Array(100).fill(true).map((item, index) => `develop-${index}`);
-        const name = nameList.find(name => form.environments.every(env => env.name !== name))!;
-        newForm.environments.push({
-          ...defaultEnv,
-          name,
-          branch: 'develop'
+  const onEdit = useCallback(
+    (
+      targetKey: string | React.MouseEvent<HTMLElement, MouseEvent>,
+      action: 'add' | 'remove'
+    ) => {
+      if (action === 'add') {
+        setForm(newForm => {
+          const nameList = new Array(100)
+            .fill(true)
+            .map((item, index) => `develop-${index}`);
+          const name = nameList.find(name =>
+            form.environments.every(env => env.name !== name)
+          )!;
+          newForm.environments.push({
+            ...defaultEnv,
+            name,
+            branch: 'develop'
+          });
+          return newForm;
         });
-        return newForm;
-      });
-    } else {
-      setForm(newForm => {
-        newForm.environments = newForm.environments.filter(item => item.name !== targetKey);
-        return newForm;
-      });
-    }
-  }, [form.environments, setForm]);
+      } else {
+        setForm(newForm => {
+          newForm.environments = newForm.environments.filter(
+            item => item.name !== targetKey
+          );
+          return newForm;
+        });
+      }
+    },
+    [form.environments, setForm]
+  );
 
-  const onChangeEnvironment = useCallback((index: number, data: IEnvsItem) => {
-    setForm(newForm => {
-      newForm.environments[index] = data;
-      return newForm;
-    });
-  }, [setForm]);
+  const onChangeEnvironment = useCallback(
+    (index: number, data: IEnvsItem) => {
+      setForm(newForm => {
+        newForm.environments[index] = data;
+        return newForm;
+      });
+    },
+    [setForm]
+  );
 
   const renderContent = useMemo(() => {
     if (loadingMap[services.ssh.getList]) return null;
-    return <Form>
-      <Form.Item label="项目名称">
-        <Input
-          {...createInput('name', { validator: Validators.required, label: '项目名称' })}
-          placeholder="项目名称"
-        />
-      </Form.Item>
+    return (
+      <Form>
+        <Form.Item label="项目名称">
+          <Input
+            {...createInput('name', {
+              validator: Validators.required,
+              label: '项目名称'
+            })}
+            placeholder="项目名称"
+          />
+        </Form.Item>
 
-      <Form.Item label="仓库路径">
-        <Input
-          {...createInput('git_path', { validator: Validators.required, label: '仓库路径' })}
-          placeholder="仓库路径"
-        />
-      </Form.Item>
-      <Tabs type="editable-card" onEdit={onEdit}>
-        {
-          form.environments.map((item, index) => (
-            <TabPane key={item.name} tab={item.name} closable={index > 0}>
-              <EnvironmentForm onChange={(data: IEnvsItem) => onChangeEnvironment(index, data)} formData={item} />
+        <Form.Item label="仓库路径">
+          <Input
+            {...createInput('git_path', {
+              validator: Validators.required,
+              label: '仓库路径'
+            })}
+            placeholder="仓库路径"
+          />
+        </Form.Item>
+        <Tabs type="editable-card" onEdit={onEdit}>
+          {form.environments.map((item, index) => (
+            <TabPane key={index} tab={item.name} closable={index > 0}>
+              <EnvironmentForm
+                onChange={(data: IEnvsItem) => onChangeEnvironment(index, data)}
+                formData={item}
+              />
             </TabPane>
-          ))
-        }
-      </Tabs>
-    </Form>;
+          ))}
+        </Tabs>
+      </Form>
+    );
   }, [createInput, form.environments, loadingMap, onChangeEnvironment, onEdit]);
-  console.log('loadingMap[services.ssh.getList]', loadingMap[services.ssh.getList]);
   return (
     <>
-
       <Modal
         title="项目 配置"
         visible={visible}
@@ -139,7 +162,9 @@ export function ProjectModal({ showBtn, formData = {
       >
         {renderContent}
       </Modal>
-      <div style={{ display: 'inline-flex' }} onClick={() => setVisible(true)}>{showBtn}</div>
+      <div style={{ display: 'inline-flex' }} onClick={() => setVisible(true)}>
+        {showBtn}
+      </div>
     </>
   );
 }
